4006c785d623bee5b7c35828d9bf32e7ac17ed2a
Annana <ananya.plainsurf@gmail.com>
2025-05-01 18:10:08 +0530

    new changes

---
+++ OOP-in-js/Apis/main.js
@@ -0,0 +1,39 @@
+// const url = "https://covid-api.com/api/regions?per_page=20";
+
+// fetch(url, {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//   },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((response) => console.log(response));
+
+const url = "https://yesno.wtf/api";
+const imgArticle = document.getElementById("img-article");
+const ansText = document.getElementById("answer-text");
+function decide(event) {
+  event.preventDefault();
+
+  fetch(url, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  }).then(async (response) => {
+    const result = await response.json();
+    if (result) {
+      const img = document.createElement("img");
+      img.setAttribute("src", result.image);
+      imgArticle.innerHTML = ""; // Clear old image
+      imgArticle.appendChild(img);
+      ansText.textContent = result.answer;
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("form").addEventListener("submit", decide);
+});
